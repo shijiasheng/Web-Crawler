ae6948f9ac7240591207323853bd9973d1acd175
tongji4m3 <1254931237@qq.com>
2020-10-21 15:46:50 +0800

    第二种情况的处理(只打印出来格式,没写入csv

--- analysis.py
+++ analysis.py
@@ -30,12 +30,12 @@ if __name__ == '__main__':
             processCount+=1
 
             # 查看特定页面问题
-            # if file!='21_B004P0N51C.html':
+            # if file!='603_B007QJAE0O.html':
             #     continue
             # 打开html文件进行操作
 
             # 避免报错后下一次用上一层的数据,造成混乱
-            text=''
+            text= ''
             title=''
             detail=''
 
@@ -59,70 +59,70 @@ if __name__ == '__main__':
                     # ['', '', '', '', 'Product details', '', '', '', '', '', 'Aspect Ratio', ':', '', '1.33:1, 1.85:1', '', '', 'Is Discontinued By Manufacturer', ':', '', 'No', '', '', 'MPAA rating', ':', '', 's_medPG13 PG-13 (Parents Strongly Cautioned)', '', '', 'Product Dimensions', ':', '', '7.5 x 5.5 x 0.53 inches; 4 Ounces', '', '', 'Item model number', ':', '', '2226213', '', '', 'Director', ':', '', 'David Raynr', '', '', 'Media Format', ':', '', 'Closed-captioned, Color, Dolby, Full Screen, NTSC, Special Edition, Subtitled', '', '', 'Run time', ':', '', '1 hour and 34 minutes', '', '', 'Release date', ':', '', 'August 1, 2000', '', '', 'Actors', ':', '', 'Shane West, James Franco, Marla Sokoloff', '', '', 'Subtitles:', ':', '', 'English', '', '', 'Producers', ':', '', 'Paul Schiff', '', '', 'Language', ':', '', 'Unqualified', '', '', 'Studio', ':', '', 'Sony Pictures Home Entertainment', '', '', 'ASIN', ':', '', 'B00003CXGJ', '', '', 'Number of discs', ':', '', '1', '', '', '', '', '', '', 'Best Sellers Rank:', '', '#76,097 in Movies & TV (See Top 100 in Movies & TV)', '', ' #5,087 in Romance (Movies & TV)', ' #10,283 in Comedy (Movies & TV)', ' #16,980 in Drama DVDs', '', '', '', '', '', '', 'Customer Reviews:', '', '', '', '', '', '', '', '4.3 out of 5 stars', '', '', '', '', '', '', '', '', '253 ratings', '', '', '', '', '', '', '', '', '', '', '', '']
 
 
-                    # while '' in title:
-                    #     title.remove('')
-                    # while '' in text:
-                    #     text.remove('')
-                    #
-                    # while ':' in text:
-                    #     text.remove(':')
-                    # while 'Product details' in text:
-                    #     text.remove('Product details')
-                    # while 'Size:One Size' in text:
-                    #     text.remove('Size:One Size')
-                    # # print(text)
-                    # # print(len(text))
-                    # index = 0
-                    # while index < len(text):
-                    #     if (text[index] == "Customer Reviews:"):
-                    #         break
-                    #     index = index + 1
-                    # if index != len(text):
-                    #     len_text = len(text)
-                    #     for i in range(len_text - 1, index - 1, -1):
-                    #         del text[i]
-                    #
-                    # index = 0
-                    # while index < len(text):
-                    #     if (text[index] == "Best Sellers Rank:"):
-                    #         break
-                    #     index = index + 1
-                    # if index != len(text):
-                    #     len_text = len(text)
-                    #     for i in range(len_text - 1, index - 1, -1):
-                    #         del text[i]
-                    # for i in range(len(text)):
-                    #     text[i] = text[i].replace('"',"'")
-                    #
-                    # for i in range(len(text)):
-                    #     if text[i]!=':':
-                    #         text[i] = text[i].replace(':','')
-                    # # print(text)
-                    # for i in range(0, len(text), 2):
-                    #     text[i] = '"' + text[i] + '":'
-                    # for i in range(2, len(text), 2):
-                    #     text[i] = ',' + text[i]
-                    # for i in range(1, len(text), 2):
-                    #     text[i] = '"' + text[i] + '"'
-                    #
-                    # for i in range(0,len(title)):
-                    #     title[i] = title[i].replace('"',"'")
-                    # # print(title[0])
-                    # # title[0] = title[0].replace('"', "'")
-                    # # print(title[0])
-                    # text.insert(0, '"title":' + '"' + title[0] + '",')
-                    # text.insert(0, '{')
-                    # text.append('}')
-                    # text = "".join(text)
-                    # text = text.replace('\\', '\\\\')
-                    # # print(text)
-                    # # print(file[0:4])
-                    # text_dict = json.loads(text)
-                    # # print(text_dict)
-                    # # texts.update(text_dict)
-                    # # dict2.update(dict1)
-                    # # texts = texts + text_dict
-                    # f_csv.writerow(text_dict)
+                    while '' in title:
+                        title.remove('')
+                    while '' in text:
+                        text.remove('')
+
+                    while ':' in text:
+                        text.remove(':')
+                    while 'Product details' in text:
+                        text.remove('Product details')
+                    while 'Size:One Size' in text:
+                        text.remove('Size:One Size')
+                    # print(text)
+                    # print(len(text))
+                    index = 0
+                    while index < len(text):
+                        if (text[index] == "Customer Reviews:"):
+                            break
+                        index = index + 1
+                    if index != len(text):
+                        len_text = len(text)
+                        for i in range(len_text - 1, index - 1, -1):
+                            del text[i]
+
+                    index = 0
+                    while index < len(text):
+                        if (text[index] == "Best Sellers Rank:"):
+                            break
+                        index = index + 1
+                    if index != len(text):
+                        len_text = len(text)
+                        for i in range(len_text - 1, index - 1, -1):
+                            del text[i]
+                    for i in range(len(text)):
+                        text[i] = text[i].replace('"',"'")
+
+                    for i in range(len(text)):
+                        if text[i]!=':':
+                            text[i] = text[i].replace(':','')
+                    # print(text)
+                    for i in range(0, len(text), 2):
+                        text[i] = '"' + text[i] + '":'
+                    for i in range(2, len(text), 2):
+                        text[i] = ',' + text[i]
+                    for i in range(1, len(text), 2):
+                        text[i] = '"' + text[i] + '"'
+
+                    for i in range(0,len(title)):
+                        title[i] = title[i].replace('"',"'")
+                    # print(title[0])
+                    # title[0] = title[0].replace('"', "'")
+                    # print(title[0])
+                    text.insert(0, '"title":' + '"' + title[0] + '",')
+                    text.insert(0, '{')
+                    text.append('}')
+                    text = "".join(text)
+                    text = text.replace('\\', '\\\\')
+                    # print(text)
+                    # print(file[0:4])
+                    text_dict = json.loads(text)
+                    # print(text_dict)
+                    # texts.update(text_dict)
+                    # dict2.update(dict1)
+                    # texts = texts + text_dict
+                    f_csv.writerow(text_dict)
 
                     # 打印
                     # print(title)
@@ -133,39 +133,81 @@ if __name__ == '__main__':
                     #         f_csv.writerow(data)
                     # print("情况1:"+file)
                 except IndexError:
+                    result=""
+
                     #     此处说明该页面不是经典的布局,启用第二种情况
                     titleSelect = "#a-page > div.av-page-desktop.avu-retail-page > div.DVWebNode-detail-atf-wrapper.DVWebNode > div > div > div._3KHiTg._2r7Wei.av-dp-container._13P0S3 > div.av-detail-section._1eXZeC > div > h1"
                     select = "#meta-info > div > dl"
                     detailSelect = "#btf-product-details > div > dl"
-                    runTimeSelect="#a-page > div.av-page-desktop.avu-retail-page > div.DVWebNode-detail-atf-wrapper.DVWebNode > div > div > div._3KHiTg._2r7Wei.av-dp-container._13P0S3 > div.av-detail-section._2ytcUH > div > div._3QwtCH._16AW_S._2LF_6p.dv-node-dp-badges.uAeEjV._1qXS7N > span:nth-child(3) > span"
+                    runTimeSelect="#a-page > div.av-page-desktop.avu-retail-page > div.DVWebNode-detail-atf-wrapper.DVWebNode > div > div > div._3KHiTg._2r7Wei.av-dp-container._13P0S3 > div.av-detail-section._1eXZeC > div > div._3QwtCH._16AW_S._2LF_6p.dv-node-dp-badges.uAeEjV._1qXS7N > span:nth-child(3) > span"
+                    typeSelect="#dv-action-box > div > div > div > div.abwJ5F.tFxybk._2LF_6p._32Y4AN > div > span._2cx-XY.dv-dp-node-watchlist._1qXS7N > form > input[type=hidden]:nth-child(2)"
+                    yearSelect="#a-page > div.av-page-desktop.avu-retail-page > div.DVWebNode-detail-atf-wrapper.DVWebNode > div > div > div._3KHiTg._2r7Wei.av-dp-container._13P0S3 > div.av-detail-section._1eXZeC > div > div._3QwtCH._16AW_S._2LF_6p.dv-node-dp-badges.uAeEjV._1qXS7N > span:nth-child(4) > span"
                     try:
                         texts = Soup.select(select)
                         titles = Soup.select(titleSelect)
                         details = Soup.select(detailSelect)
+                        types=Soup.select(typeSelect)
+                        runTime = Soup.select(runTimeSelect)
+                        year=Soup.select(yearSelect)
 
-                        # 占位,区分异常情况
-                        text=texts[0].text
 
-                        runTime=Soup.select(runTimeSelect)
-                        # if len(runTime)!=0:
-                        #     print(runTime[0].text)
+                        # 还可以区分异常情况
+                        text = texts[0].text
+                        title=titles[0].text
+
+                        result+="title:"+title+","
+
+                        if "titleType=movie" in repr(types[0]):
+                            # 必然是movie
+                            result+="isMovie:true,"
+                            # movieType=repr(types[0])
+                            # index=movieType.find("titleType=")
+                            # print(movieType[index:index+20])
+                        if len(runTime) != 0:
+                            # 有运行时间
+                            result += "runTime:"+runTime[0].text+","
+                            # print(runTime[0].text)
+                        if len(year)!=0:
+                            result += "year:" + year[0].text + ","
+
+
+
+
                         # print(texts[0].text)
                         # print(texts[0].text.split('\n'))
                         # text = texts[0].text.splitlines()
                         # title = titles[0].text.splitlines()
                         # detail=details[0].text.splitlines()
 
-                        # for text in texts:
-                        #     print(text.text)
+                        for text in texts:
+                            text=text.text
+                            if "Directors" in text:
+                                text=text.replace("Directors","")
+                                result += "Directors:" + text + ","
+                            if "Starring" in text:
+                                text=text.replace("Starring","")
+                                result += "Starring:" + text + ","
+                            if "Genres" in text:
+                                text=text.replace("Genres","")
+                                result += "Genres:" + text + ","
+
+                        for detail in details:
+                            text = detail.text
+                            if "Supporting actors" in text:
+                                text=text.replace("Supporting actors","")
+                                result += "Supporting actors:" + text + ","
+                                print(text)
                         # for detail in details:
                         #     print(detail.text)
                         # temp=texts[0].text
                         # print("情况2:"+file)
+
+                        print(result)
                     except IndexError:
                         # 此处说明该页面不是经典的布局,启用第三种情况
                         # 未知,先打印文件名,手动查看,编写代码
                         errorCount+=1
-                        print(errorCount,processCount,sep='/')
+                        # print(errorCount,processCount,sep='/')
                         # print(file)
                         # question_files.append(file)
             # 去除空元素
